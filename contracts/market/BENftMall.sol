ba3782e58275483d3128541ae1e445647e08d081
CounterFire2023 <136581895+CounterFire2023@users.noreply.github.com>
2023-06-16 12:51:53 +0800

    Update BENftMall.sol
    remove some unused import

--- contracts/market/BENftMall.sol
+++ contracts/market/BENftMall.sol
@@ -1,11 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/governance/TimelockController.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "../core/HasSignature.sol";
 import "../interfaces/IBEERC721.sol";
